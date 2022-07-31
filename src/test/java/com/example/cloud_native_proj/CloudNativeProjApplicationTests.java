449f630b3d5f93e55edf2ceacb43bc86abe0a96e
HuiyuanYan <92134290+HuiyuanYan@users.noreply.github.com>
2022-07-31 22:50:10 +0800

    Update CloudNativeProjApplicationTests.java
    单元测试内容

--- src/test/java/com/example/cloud_native_proj/CloudNativeProjApplicationTests.java
+++ src/test/java/com/example/cloud_native_proj/CloudNativeProjApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.cloud_native_proj;
-
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+@SpringBootTest(classes = CloudNativeProjApplication.class)
 class CloudNativeProjApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/hello")//使用get方式来调用接口
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+		).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		System.out.println(mvcResult.getResponse().getContentAsString());
 	}
 
 }
